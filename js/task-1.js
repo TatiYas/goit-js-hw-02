b4d32ee4515a48dafbbfe427ed27509f001db00b
TatiYas <iasinovska_lawyer@ukr.net>
2024-03-18 00:27:52 +0100

    First

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function makeTransaction(orderedQuantity, pricePerItem) {
+    const totalPrice = calculateTotalPrice(orderedQuantity, pricePerItem);
+    return `You ordered ${orderedQuantity} droids worth ${totalPrice} credits!`;
+  }
+  
+  function calculateTotalPrice(orderedQuantity, pricePerItem) {
+    return orderedQuantity * pricePerItem;
+  }
+  
+  console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+  console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+  console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
