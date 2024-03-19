5afa2d2020686b45f85707bfe6640747968ece22
TatiYas <iasinovska_lawyer@ukr.net>
2024-03-19 12:57:57 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,12 +2,12 @@
   
 function getElementWidth(content, padding, border) {
     const result = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return (result);
+    return (result);}
   
     console.log(getElementWidth("50px", "8px", "4px")); // 74
     console.log(getElementWidth("60px", "12px", "8.5px")); // 101
     console.log(getElementWidth("200px", "0px", "0px")); // 200
-}
+
 
 
 //Завдання #3 вирішено неправильно
