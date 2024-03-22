9b51f90353e0ae2775b45d1c385294116c245237
TatiYas <iasinovska_lawyer@ukr.net>
2024-03-22 14:04:01 +0100

    Update 1

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,19 @@
-//function getElementWidth(content, padding, border) {const contentWidth = parseInt(content.slice(0, -2))const paddingWidth = parseInt(padding.slice(0, -2)) * 2;const borderWidth = parseInt(border.slice(0, -2)) * 2;return contentWidth + paddingWidth + borderWidth;}//
-  
-function getElementWidth(content, padding, border) {
-    const result = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
-    return (result);}
-  
-    console.log(getElementWidth("50px", "8px", "4px")); // 74
-    console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-    console.log(getElementWidth("200px", "0px", "0px")); // 200
+//Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+
+//Доповни код функції таким чином, що:
+//Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+//Якщо в рядку відсутні заборонені слова, функція повертає буль false
 
 
-
-//Завдання #3 вирішено неправильно
-
-//Проблема була частково вирішена правильно, але є покращення, які потрібно внести.Критичні помилки:Вам слід використовувати parseFloat замість parseInt, оскільки parseInt не зможе обробити десяткові дроби, які є допустимими вводами відповідно до вимог завдання, наприклад, у тестовому випадку "8.5px". Використовуючи parseFloat, ви гарантуєте, що функція зможе правильно обробляти значення з десятковими дробами.Є невелике покращення у способі видалення 'px' із рядків. Хоча slice(0, -2) працює, це менш гнучкий метод і базується на припущенні, що всі одиниці будуть рівно двох символів завдовжки.
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+    }
+    
+    console.log(checkForSpam("Latest technology news")); // false
+    console.log(checkForSpam("JavaScript weekly newsletter")); // false
+    console.log(checkForSpam("Get best sale offers now!")); // true
+    console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+    console.log(checkForSpam("Trust me, this is not a spam message")); // true
+    console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+    console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
